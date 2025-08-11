d3d596ad04de9437e3c722f6b0b3d4de95f6042f
Bayuk <bayukkominfo@gmail.com>
2025-08-11 17:44:26 +0700

    Users : Migration & Seed

--- database/migrations/2025_08_11_090000_create_users_table.php
+++ database/migrations/2025_08_11_090000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('phone_number')->unique();
+            $table->string('avatar')->nullable();
+            $table->integer('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
+            'address' => 'Alamat Test',
+            'phone_number' => '085854445232',
+            'avatar' => 'user-placeholder.png',
+            'role_id' => '1'
         ]);
     }
 }
